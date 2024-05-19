c029eb0e2f136f9100d87ec2e658fd0fa21d464d
Exotigrano <exotigrano@yandex.ru>
2024-05-19 19:47:17 +0300

    finish

---
+++ build/tests/CMakeFiles/runTests.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runTests.dir/tt.cpp.o"
+  "CMakeFiles/runTests.dir/tt.cpp.o.d"
+  "runTests"
+  "runTests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/runTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/steve-kameron/Загрузки/DesignPattern/factory/tests
+# Build directory: /home/steve-kameron/Загрузки/DesignPattern/factory/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(runTests "/home/steve-kameron/Загрузки/DesignPattern/factory/build/tests/runTests")
+set_tests_properties(runTests PROPERTIES  _BACKTRACE_TRIPLES "/home/steve-kameron/Загрузки/DesignPattern/factory/tests/CMakeLists.txt;22;add_test;/home/steve-kameron/Загрузки/DesignPattern/factory/tests/CMakeLists.txt;0;")
+subdirs("../_deps/googletest-build")
